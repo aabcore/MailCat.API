0de2cca50fcbb06fc4b7cc2915b6aa9dccafb577
Pearse Hutson <pearse.2008@gmail.com>
2020-07-21 18:12:39 -0500

    Adding health controller.

---
+++ MailCat.API/Controllers/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MailCat.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+    }
+}
